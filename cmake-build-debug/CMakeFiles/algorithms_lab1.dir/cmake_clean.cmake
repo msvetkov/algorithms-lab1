3fb52a0f8db2a2e40b101b670ba9bb25107ff757
msvetkov <44121460+msvetkov@users.noreply.github.com>
2019-09-28 12:48:01 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/algorithms_lab1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithms_lab1.dir/main.cpp.obj"
+  "algorithms_lab1.pdb"
+  "algorithms_lab1.exe"
+  "algorithms_lab1.exe.manifest"
+  "libalgorithms_lab1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algorithms_lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
